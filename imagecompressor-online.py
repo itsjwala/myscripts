50807c81a0cd879120d36acf54e79e4ab647a0d9
Jigar wala <jigar.wala@somaiya.edu>
2018-11-27 20:49:33 +0530

    Update imagecompressor-online.py

--- imagecompressor-online.py
+++ imagecompressor-online.py
@@ -1,24 +1,22 @@
-import requests
-import json
-
 # X-rayed https://compressor.io/compress 
 # image format supported jpeg, png, gif, jpeg
 
-# for lossless compression
-url = "https://compressor.io/server/Lossless.php"
+import requests
+import json
+from os import path
 
-# for lossy compression
-# url = "https://compressor.io/server/Lossy.php"
 
-file_name = "sample.png"
+def compress(file_path,lossy = True):
+	url = "https://compressor.io/server/Lossy.php" if lossy else "https://compressor.io/server/Lossless.php" 
+	session = requests.Session()
+	file_name = path.split(file_path)[-1]
 
-session = requests.Session()
+	response = session.post(url,files ={"files" :  (file_name,open(file_path,"rb"))})
+	response = json.loads(response.content)
+	res = session.get(response["files"][0]["url"])
+	with open(f"compressed-{file_name}","wb") as f:
+	    f.write(res.content)
 
-response = session.post(url,files ={"files" :  (file_name,open(file_name,"rb"))})
 
-response = json.loads(response.content)
 
-res = session.get(response["files"][0]["url"])
-
-with open(f"compressed-{file_name}","wb") as f:
-    f.write(res.content)
+compress("sample.png")	
