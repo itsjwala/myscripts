208cb2c96d7f487511f892dddf0868773877b80e
jigar wala <jigarwala007@gmail.com>
2019-12-03 18:52:52 +0530

    added cmd args in image compressor script

--- README.md
+++ README.md
@@ -2,10 +2,14 @@
 
 Script List :-
 
-* [Compress Images using online service.](https://github.com/jigarWala/myscripts/blob/master/imagecompressor-online.py)
+* [Compress Images using online service.](https://github.com/jigarWala/myscripts/blob/master/imagecompressor-online.py)<br>
+  Eg.
+  ```
+  	python3 imagecompressor-online.py location/of/image [lossy|notlossy]
+  ```
 * [log tail of file](https://github.com/jigarWala/myscripts/blob/master/tail-n.py)<br>
   Eg.
-  ```                     
+  ```
     python3 tail-n.py  10  FILENAME
     python3 tail-n.py  [f/F]10  FILENAME
     python3 tail-n.py  10[f/F]  FILENAME
--- imagecompressor-online.py
+++ imagecompressor-online.py
@@ -1,22 +1,40 @@
-# X-rayed https://compressor.io/compress 
+# X-rayed https://compressor.io/compress
 # image format supported jpeg, png, gif, jpeg
 
 import requests
 import json
 from os import path
+import sys
 
+def usage():
+	u = f"""
+python3 {__file__} FILEPATH [lossy|notlossy]
+	"""
+	print(u)
 
 def compress(file_path,lossy = True):
-	url = "https://compressor.io/server/Lossy.php" if lossy else "https://compressor.io/server/Lossless.php" 
+
+	url = "https://compressor.io/server/Lossy.php" if lossy else "https://compressor.io/server/Lossless.php"
 	session = requests.Session()
 	file_name = path.split(file_path)[-1]
-
 	response = session.post(url,files ={"files" :  (file_name,open(file_path,"rb"))})
 	response = json.loads(response.content)
 	res = session.get(response["files"][0]["url"])
-	with open(f"compressed-{file_name}","wb") as f:
+	with open(path.join(path.dirname(file_path),f"compressed-{file_name}"),"wb") as f:
 	    f.write(res.content)
 
 
+try:
+	filepath = sys.argv[1]
 
-compress("sample.png")	
+	lossy = True if sys.argv[2] == "lossy" else False
+
+	print("Processing...")
+	compress(file_path=filepath,lossy=lossy)
+	print("Done!")
+except IndexError:
+	usage()
+except Exception as e:
+	print(e)
+
+
